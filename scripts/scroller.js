4c62a13b223f566e0a09533bfae94bc9540393a5
surajgoel5 <goel.suraj5@gmail.com>
2020-12-03 07:32:55 +0530

    finsihed almost

--- scripts/scroller.js
+++ scripts/scroller.js
@@ -1,14 +1,62 @@
-function isElementInViewport(elem) {
-    var $elem = $(elem);
+$(document).ready(function(){
+	type("p1text");
+	})
 
-    // Get the scroll position of the page.
-    var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
-    var viewportTop = $(scrollElem).scrollTop();
-    var viewportBottom = viewportTop + $(window).height();
-
-    // Get the position of the element on the page.
-    var elemTop = Math.round( $elem.offset().top );
-    var elemBottom = elemTop + $elem.height();
-
-    return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
-}
+var p2typed=false;
+var p3typed=false;
+var p4typed=false;
+$(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+	
+	if (scroll<1.1*$(window).height()){
+	var newW=$( window ).width()*scroll/$( window ).height();
+		$("#hider1").css("width",newW*1.05);
+		$("#hider2").css("width",0);
+		$("#hider3").css("width",0);
+		}
+	
+	else if (scroll<2.1*$(window).height()){
+	var newW=$( window ).width()*(scroll-1.1*$( window ).height())/($( window ).height());
+	newW = newW < 0 ? 0 : newW;
+	$("#hider1").css("width",$( window ).width()*1.05);
+	
+		$("#hider2").css("width",newW*1.1);
+		
+		$("#hider3").css("width",0);
+		}
+	else if (scroll<3.1*$(window).height()){
+		var newW=$( window ).width()*(scroll-2.1*$( window ).height())/($( window ).height());
+	newW = newW < 0 ? 0 : newW;
+		$("#hider3").css("width",newW*2.1);
+		
+		$("#hider1").css("width",$( window ).width()*1.05);
+	
+		$("#hider2").css("width",$( window ).width()*1.1);
+		
+		
+		}
+		
+		
+	if (scroll>$(window).height()){
+		if(!p2typed){
+		p2typed=true;
+		type("p2text");
+		}
+		}
+	if (scroll>2*$(window).height()){
+			if(!p3typed){
+		p3typed=true;
+		type("p3text");
+		}
+			}
+	if (scroll>3*$(window).height()){
+			if(!p4typed){
+		p4typed=true;
+		type("p4text");
+		}	
+				
+	}
+	
+	
+    
+});
