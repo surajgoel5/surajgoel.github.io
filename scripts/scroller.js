09f1f7b7b99fe483cee6cb67e4d9b3f6a88715af
surajgoel5 <goel.suraj5@gmail.com>
2020-12-10 23:32:44 +0530

    scroller fixes

--- scripts/scroller.js
+++ scripts/scroller.js
@@ -10,48 +10,64 @@ var lasttyped=false;
 $(window).scroll(function (event) {
     var scroll = $(window).scrollTop();
 	var H=$(window).height();
-	if (scroll<1.1*H && scroll>0.1*H){
-	var newW=$( window ).width()*scroll/$( window ).height()-0.1;
-		$("#hider1").css("width",newW*1.1);
+	var W=$( window ).width();
+	if (scroll<0.9*H && scroll>0*H){
+	var newW=W*scroll/H;
+		$("#hider1").css("width",newW*1.3);
 		$("#hider2").css("width",0);
 		$("#hider3").css("width",0);
 		$("#hider4").css("width",0);
 		}
-	
-	else if (scroll<2.1*H && scroll>1.15*H){
-	var newW=$( window ).width()*(scroll-1.1*$( window ).height())/($( window ).height());
+	else if (scroll>=0.9*H && scroll<=1.1*H){
+		$("#hider1").css("width",W*1.3);
+		$("#hider2").css("width",0);
+		$("#hider3").css("width",0);
+		$("#hider4").css("width",0);
+		}
+	else if (scroll<1.9*H && scroll>1.1*H){
+	var newW=$( window ).width()*(scroll-1.1*H)/H;
 	newW = newW < 0 ? 0 : newW;
 	
-	$("#hider1").css("width",$( window ).width()*1.1);
-	$("#hider2").css("width",newW*1.1);
+	$("#hider1").css("width",$( window ).width()*1.3);
+	$("#hider2").css("width",newW*1.3);
 	$("#hider3").css("width",0);
 	$("#hider4").css("width",0);
 		}
 		
-		
-	else if (scroll<3.1*H && scroll>2.15*H){
-		var newW=$( window ).width()*(scroll-2.1*$( window ).height())/($( window ).height());
+	else if (scroll>=1.9*H && scroll<=2.1*H){
+		$("#hider1").css("width",W*1.3);
+		$("#hider2").css("width",W*1.3);
+		$("#hider3").css("width",0);
+		$("#hider4").css("width",0);
+		}	
+	else if (scroll<2.9*H && scroll>2.1*H){
+		var newW=$( window ).width()*(scroll-2.1*H)/H;
 	newW = newW < 0 ? 0 : newW;
 	
 	$("#hider3").css("width",0);
-	$("#hider1").css("width",$( window ).width()*1.1);
-	$("#hider2").css("width",$( window ).width()*1.1);
-	$("#hider4").css("width",newW*1.1);
+	$("#hider1").css("width",$( window ).width()*1.3);
+	$("#hider2").css("width",$( window ).width()*1.3);
+	$("#hider4").css("width",newW*1.3);
 		
 		
 		}
+	else if (scroll>=2.9*H && scroll<=3.1*H){
+		$("#hider1").css("width",W*1.3);
+		$("#hider2").css("width",W*1.3);
+		$("#hider3").css("width",0);
+		$("#hider4").css("width",W*1.3);
+		}	
 		
 		
 		
-		
-	else if (scroll<4.1*H && scroll>3.15*H){
-		var newW=$( window ).width()*(scroll-3.1*$( window ).height())/($( window ).height());
+	else if (scroll<3.9*H && scroll>3.1*H){
+		var newW=$( window ).width()*(scroll-3.1*H)/H;
 	newW = newW < 0 ? 0 : newW;
 	
-	$("#hider3").css("width",newW*1.1);
-	$("#hider1").css("width",$( window ).width()*1.1);
-	$("#hider2").css("width",$( window ).width()*1.1);
-	$("#hider4").css("width",$( window ).width()*1.1);	
+	$("#hider3").css("width",newW*1.3);
+	$("#hider1").css("width",$( window ).width()*1.3);
+	$("#hider2").css("width",$( window ).width()*1.3);
+	$("#hider4").css("width",$( window ).width()*1.3);	
 		
 		}
 		
