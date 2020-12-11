99e3ebf04689ef15383ad2c34f2958388e3a850b
surajgoel5 <goel.suraj5@gmail.com>
2020-12-12 02:42:54 +0530

    faster typing

--- scripts/typer.js
+++ scripts/typer.js
@@ -1,5 +1,5 @@
 //Usage type(id);
-
+//MAth.random muliplier to and add to change to slow or speed up
 
 function type(id){
 setTimeout(function foo1(){SPY.text(id,id);},0);
@@ -9,12 +9,12 @@ setTimeout(function foo1(){SPY.text(id,id);},0);
 var SPY = function() {
   function e(a, d, b) {
     var c, f, g, h;
-    b == a.length ? k.animationComplete = !0 : (g = d.innerHTML, h = Math.floor(21 * Math.random() + 5), c = 32 === a[b] ? 32 : a[b] - h, f = setInterval(function() {
+    b == a.length ? k.animationComplete = !0 : (g = d.innerHTML, h = Math.floor(8 * Math.random() + 0), c = 32 === a[b] ? 32 : a[b] - h, f = setInterval(function() {
       d.innerHTML = g + String.fromCharCode(c);
       c == a[b] ? (clearInterval(f), c = 32, b++, setTimeout(function() {
         e(a, d, b);
-      }, 10)) : c++;
-    }, 5));
+      }, 0)) : c++;
+    }, 0));
   }
   var k = {};
   return k = {animationComplete:!1, text:function(a,old) {
